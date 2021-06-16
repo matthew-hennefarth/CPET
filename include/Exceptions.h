3873c47d69bd9531412bba03fa918ee9fe2f67fe
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2021-06-16 12:02:40 -0700

    Updated Exceptions perfect forwarding

--- include/Exceptions.h
+++ include/Exceptions.h
@@ -14,8 +14,9 @@ namespace cpet {
 class exception : public std::runtime_error {
  public:
   template <typename StringType,
-            typename = typename std::enable_if<
-                std::is_convertible_v<StringType, std::string>>>
+            typename = std::enable_if_t<
+                std::is_convertible_v<StringType, std::string>>,
+            typename = std::enable_if_t<!std::is_array_v<StringType>>>
   explicit exception(StringType&& what_arg)
       : std::runtime_error(std::forward<StringType>(what_arg)) {
   }  // NOLINT(hicpp-no-array-decay,cppcoreguidelines-pro-bounds-array-to-pointer-decay)
@@ -24,33 +25,39 @@ class exception : public std::runtime_error {
 class value_error : public cpet::exception {
  public:
   template <typename StringType,
-            typename = typename std::enable_if<
-                std::is_convertible_v<StringType, std::string>>>
+            typename = std::enable_if_t<
+                std::is_convertible_v<StringType, std::string>>,
+            typename = std::enable_if_t<!std::is_array_v<StringType>>>
   explicit value_error(StringType&& what_arg)
       : cpet::exception(std::forward<StringType>(what_arg)) {}
 };
 
 class value_not_found : public cpet::exception {
  public:
-  template <typename S1, typename = typename std::enable_if<
-                             std::is_convertible_v<S1, std::string>>>
-  explicit value_not_found(S1&& what_arg)
-      : cpet::exception(std::forward<S1>(what_arg)) {}
+  template <typename StringType,
+            typename = std::enable_if_t<
+                std::is_convertible_v<StringType, std::string>>,
+            typename = std::enable_if_t<!std::is_array_v<StringType>>>
+  explicit value_not_found(StringType&& what_arg)
+      : cpet::exception(std::forward<StringType>(what_arg)) {}
 };
 
 class io_error : public cpet::exception {
  public:
-  template <typename S1, typename = typename std::enable_if<
-                             std::is_convertible_v<S1, std::string>>>
-  explicit io_error(S1&& what_arg)
-      : cpet::exception(std::forward<S1>(what_arg)) {}
+  template <typename StringType,
+            typename = std::enable_if_t<
+                std::is_convertible_v<StringType, std::string>>,
+            typename = std::enable_if_t<!std::is_array_v<StringType>>>
+  explicit io_error(StringType&& what_arg)
+      : cpet::exception(std::forward<StringType>(what_arg)) {}
 };
 
 class invalid_option : public cpet::exception {
  public:
   template <typename StringType,
-            typename = typename std::enable_if<
-                std::is_convertible_v<StringType, std::string>>>
+            typename = std::enable_if_t<
+                std::is_convertible_v<StringType, std::string>>,
+            typename = std::enable_if_t<!std::is_array_v<StringType>>>
   explicit invalid_option(StringType&& what_arg)
       : cpet::exception(std::forward<StringType>(what_arg)) {}
 };
